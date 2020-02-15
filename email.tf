bc537d1287db4e15caf8949d8e67c8a5e538e88c
pgfisico <pgfisico.dev@gmail.com>
2020-02-15 13:10:03 -0700

    Fix config block (#3)
    * Add config block
    
    * Fix config syntax
    
    * Another missing equals
    
    * Fix remote state output
    
    * Add trailing dot

--- email.tf
+++ email.tf
@@ -1,14 +1,14 @@
 resource "aws_route53_record" "email_mx" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = ""
   type    = "MX"
   ttl     = 300
-  records = ["10 mail.intimitrons.ca"]
+  records = ["10 mail.intimitrons.ca."]
 }
 
 // mail.intimitrons.ca is currently in use by mail clients
 resource "aws_route53_record" "email_mail" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = "mail"
   type    = "A"
   ttl     = 300
@@ -18,7 +18,7 @@ resource "aws_route53_record" "email_mail" {
 // webmail.intimitrons.ca accesses the web client
 // Likely unused by most on the team but maintained for now to ensure there is always a way to access email
 resource "aws_route53_record" "email_webmail" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = "webmail"
   type    = "A"
   ttl     = 300
@@ -26,7 +26,7 @@ resource "aws_route53_record" "email_webmail" {
 }
 
 resource "aws_route53_record" "email_spf" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = ""
   type    = "TXT"
   ttl     = 300
@@ -35,7 +35,7 @@ resource "aws_route53_record" "email_spf" {
 
 // Private key can be retrieved from cPanel
 resource "aws_route53_record" "email_dkim" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = "default._domainkey"
   type    = "TXT"
   ttl     = 300
--- main.tf
+++ main.tf
@@ -5,16 +5,18 @@ provider "aws" {
 
 data "terraform_remote_state" "dns" {
   backend = "remote"
-  hostname = "app.terraform.io"
-  organization = "intimitrons"
-  workspaces {
-    name = "dns"
+  config = {
+    hostname = "app.terraform.io"
+    organization = "intimitrons"
+    workspaces = {
+      name = "dns"
+    }
   }
 }
 
 // Ensure we maintain easy access to cPanel until we migrate off StableHost
 resource "aws_route53_record" "cpanel" {
-  zone_id = data.terraform_remote_state.dns.zone_id
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = "cpanel"
   type    = "A"
   ttl     = 300
